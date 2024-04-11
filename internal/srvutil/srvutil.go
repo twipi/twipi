83fd4ced57a35a91a8b9707c590a098635640314
diamondburned <diamond@libdb.so>
2024-04-11 15:33:14 -0700

    Add srvutil.TryHandlers

--- internal/srvutil/srvutil.go
+++ internal/srvutil/srvutil.go
@@ -13,3 +13,39 @@ func ParseForm(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 	})
 }
+
+// TryHandlers returns a handler that tries each handler in order until one
+// returns a non-404 status code.
+func TryHandlers(handlers ...http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		for _, h := range handlers {
+			hw := &headerWriter{w: w}
+			h.ServeHTTP(hw, r)
+			if hw.status != 404 {
+				return
+			}
+		}
+		http.NotFound(w, r)
+	})
+}
+
+type headerWriter struct {
+	w      http.ResponseWriter
+	status int
+}
+
+func (h *headerWriter) Header() http.Header {
+	return h.w.Header()
+}
+
+func (h *headerWriter) Write(b []byte) (int, error) {
+	if h.status == 404 {
+		// prevent writing a body for 404 responses
+		return len(b), nil
+	}
+	return h.w.Write(b)
+}
+
+func (h *headerWriter) WriteHeader(status int) {
+	h.status = status
+}
